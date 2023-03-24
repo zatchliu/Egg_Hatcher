37755052aa2601aada50fc12c0588239a392f107
zatchliu <yzliu819@gmail.com>
2023-03-24 19:20:21 -0400

    oki

---
+++ egg_hatcher/egg_hatcher.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+class User:
+    def __init__(self):
+        """
+        Constructor for User class
+        """
+
+class Hatcher
+    def __init__(self):
+        """
+        Constructor for Hatcher class
+
+        Attributes"
+            count: the current number of eggs on the screen
+        """
+    self.count = 0 
+
+    def __len__(self):
+        """
+        returns number of eggs
+        """
+        return self.count
+
+    def lay_egg():
+        """
+        Adds an egg to the screen
+        """
+        self.count += 1
+
+    def hatch_eggs():
+        """
+        Hatches all the eggs
+        """
+        self.count = 0
